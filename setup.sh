0081ba0841177009df055822ed2186cfae9b6d54
Fatma <fatmasherif98@yahoo.com>
2020-11-13 13:55:35 +0200

    changed db url

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export AUTH0_DOMAIN="casting-agency1.us.auth0.com"
 export API_AUDIENCE="castings"
 export ALGORITHMS=["RS256"]
-export DATABASE_URL="postgres://postgres:postgres@localhost:5432/castingagency"
+export DATABASE_URL="postgres://ebfjopyzzqfitp:6c82fc39770dc822bb5cbb1c74c44673c38864b1bdbebeba43f74667219dd5dc@ec2-3-220-222-72.compute-1.amazonaws.com:5432/dc9ov115ugm4lm"
